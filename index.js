5a72e7d92d1b4b681bd9e769375a8bee93805ec6
aliir74 <aliirani74@gmail.com>
2018-02-09 19:55:45 +0330

    create userSchema and check twice poll

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const TelegramBot = require('node-telegram-bot-api');
 var mongoose = require('mongoose');
 var fs = require('fs');
 
+var strings = {
+    'twice': "شما قبلا این نامه را امضا کرده‌اید :)",
+}
+
+
 var tocken
 fs.readFile('tocken.txt', 'utf8', function (err, data) {
     if (err) {
@@ -20,3 +25,37 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('db connect')
 });
+
+
+var userSchema = mongoose.Schema({
+    chatId: Number,
+    name: String,
+    typeOfConnection: String,
+    Email: String,
+    School: String,
+    University: String,
+    Description: String,
+});
+
+var userModel = mongoose.model('userModel', userSchema)
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id
+    userModel.findOne({'chatId': chatId}, function (err, data) {
+        if(err)
+            throw err
+        if(doc) {
+           bot.sendMessage(chat_id, strings['twice'])
+        } else {
+            startForm();
+        }
+    })
+})
+
+function startFrom() {
+
+
+}
