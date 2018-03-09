94e56b3d9473d6786848ab125e157c4db0b11f13
aliir74 <aliirani74@gmail.com>
2018-03-09 20:43:37 +0330

    add username to db

---
+++ getUsername.js
@@ -0,0 +1,57 @@
+const TelegramBot = require('node-telegram-bot-api');
+var mongoose = require('mongoose');
+var fs = require('fs');
+
+var token = "527551241:AAFu1UusFdUSp8k07dGceIk4LKgme6B3gJI"
+/*fs.readFile('token.txt', 'utf8', function (err, data) {
+    if (err) {
+        console.log('token read error!')
+    }
+    token = data;
+    addUsernames();
+    console.log(token)
+});*/
+
+mongoose.connect('mongodb://localhost/savesampad');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('db connect')
+});
+
+var userSchema = mongoose.Schema({
+  chatId: Number,
+  username: String,
+  first_name: String,
+  last_name: String,
+  name: String,
+  typeOfConnection: String,
+  email: String,
+  school: String,
+  university: String,
+  description: String,
+  state: Number,
+});
+
+var userModel = mongoose.model('userModel', userSchema)
+addUsernames()
+
+function addUsernames() {
+  const bot = new TelegramBot(token, {polling: true});
+  userModel.find({}, function (err, users) {
+    if(!users) {
+      return
+    }
+    for(let i = 0; i < users.length; i++) {
+      let chatId = users[i].chatId
+      bot.getChat(chatId, function(msg) {
+        console.log(msg)
+        users[i].username = msg.chat.username
+        users[i].save()
+      })
+    }
+  })
+}
